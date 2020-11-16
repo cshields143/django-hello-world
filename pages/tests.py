0985930e4cce8f66113e034adf47a38ed6e2896e
Chris Shields <christopher.shields143@gmail.com>
2020-11-16 12:11:40 -0800

    Create unit tests to confirm home/about pages load

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+
+class PagesTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        rsp = self.client.get('/')
+        self.assertEqual(rsp.status_code, 200)
+    def test_about_page_status_code(self):
+        rsp = self.client.get('/about/')
+        self.assertEqual(rsp.status_code, 200)
